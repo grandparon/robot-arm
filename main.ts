feba5f71e0c507dbee64b96da032f04af05c4908
GrandPa <75545155+grandparon@users.noreply.github.com>
2021-05-26 22:29:16 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.showIcon(IconNames.Heart)
+let item = 0
+let flag = 5
+basic.forever(function () {
+    if (flag + item > 180) {
+        flag = -2
+    }
+    if (flag + item < 0) {
+        flag = 2
+    }
+    item += flag
+    for (let i = 0; i <= 15; i++) {
+        Servo.Servo(i, item)
+    }
+    basic.pause(2)
+})
